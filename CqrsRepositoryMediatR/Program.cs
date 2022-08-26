7f0ae50e4d00359613e8ad4830aa9e55187a77c2
FerideColak <feride_colak577@hotmail.com>
2022-08-26 16:23:15 +0300

    IoC Container'a servisler eklendi.

--- CqrsRepositoryMediatR/Program.cs
+++ CqrsRepositoryMediatR/Program.cs
@@ -1,8 +1,21 @@
+using MediatR;
+using CqrsRepositoryMediatR.Repositories;
+using AutoMapper;
+using CqrsRepositoryMediatR.Mapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddMediatR(typeof(Program));
+builder.Services.AddSingleton<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
+
+var config = new MapperConfiguration(conf =>
+{
+    conf.AddProfile<MappingProfile>();
+});
+builder.Services.AddScoped(s => config.CreateMapper());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
