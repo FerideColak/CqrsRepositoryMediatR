7941aff87c1a2476b7a86d97a5f85fa4b7066600
FerideColak <feride_colak577@hotmail.com>
2022-08-26 16:22:46 +0300

    MappingProfile sınıfı oluşturuldu.

---
+++ CqrsRepositoryMediatR/Mapper/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CqrsRepositoryMediatR.Cqrs.Commands.Requests;
+using CqrsRepositoryMediatR.Cqrs.Commands.Responses;
+using CqrsRepositoryMediatR.Cqrs.Queries.Responses;
+using CqrsRepositoryMediatR.Entities;
+
+namespace CqrsRepositoryMediatR.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, UpdateProductCommandResponse>();
+            CreateMap<Product, GetProductByIdQueryResponse>();
+            CreateMap<Product, GetProductsQueryResponse>();
+            CreateMap<CreateProductCommandRequest, Product>();
+            CreateMap<UpdateProductCommandRequest, Product>();
+        }
+
+    }
+}
