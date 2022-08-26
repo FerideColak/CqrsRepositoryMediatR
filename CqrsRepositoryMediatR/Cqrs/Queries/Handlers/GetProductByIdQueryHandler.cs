1b6753ec4f05489a71eeea56ae744a78d81a68db
FerideColak <feride_colak577@hotmail.com>
2022-08-26 16:20:54 +0300

    QueryHandler sınıfları oluşturuldu.

---
+++ CqrsRepositoryMediatR/Cqrs/Queries/Handlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CqrsRepositoryMediatR.Cqrs.Queries.Requests;
+using CqrsRepositoryMediatR.Cqrs.Queries.Responses;
+using CqrsRepositoryMediatR.Repositories;
+using MediatR;
+
+namespace CqrsRepositoryMediatR.Cqrs.Queries.Handlers
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQueryRequest, GetProductByIdQueryResponse>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public GetProductByIdQueryHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public Task<GetProductByIdQueryResponse> Handle(GetProductByIdQueryRequest request, CancellationToken cancellationToken)
+        {
+            var product = _productRepository.GetProductById(request.Id);
+            var mapProduct = _mapper.Map<GetProductByIdQueryResponse>(product);
+            return Task.FromResult(mapProduct);
+        }
+    }
+}
---
+++ CqrsRepositoryMediatR/Cqrs/Queries/Handlers/GetProductsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CqrsRepositoryMediatR.Cqrs.Queries.Requests;
+using CqrsRepositoryMediatR.Cqrs.Queries.Responses;
+using CqrsRepositoryMediatR.Repositories;
+using MediatR;
+
+namespace CqrsRepositoryMediatR.Cqrs.Queries.Handlers
+{
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQueryRequest, List<GetProductsQueryResponse>>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public GetProductsQueryHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public Task<List<GetProductsQueryResponse>> Handle(GetProductsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var productList = _productRepository.GetAllProducts();
+            var mapProducts = _mapper.Map<List<GetProductsQueryResponse>>(productList);
+            return Task.FromResult(mapProducts);
+        }
+    }
+}
