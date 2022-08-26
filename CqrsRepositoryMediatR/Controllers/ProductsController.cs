434133b4f9322812735b58f9c70e2ea7565f9ef5
FerideColak <feride_colak577@hotmail.com>
2022-08-26 16:20:21 +0300

    ProductsController sınıfı eklendi.

---
+++ CqrsRepositoryMediatR/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using CqrsRepositoryMediatR.Cqrs.Commands.Requests;
+using CqrsRepositoryMediatR.Cqrs.Queries.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CqrsRepositoryMediatR.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await _mediator.Send(new GetProductsQueryRequest());
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            await _mediator.Send(new GetProductByIdQueryRequest { Id = id }); ;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _mediator.Send(new DeleteProductCommandRequest { Id = id});
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, UpdateProductCommandRequest request)
+        {
+            request.Id = id;
+            await _mediator.Send(request);
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommandRequest request)
+        {
+            await _mediator.Send(request);
+            return Ok();
+        }
+    }
+}
