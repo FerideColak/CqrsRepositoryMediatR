84412a8837cb242d6223695725d06c00d4edb78b
FerideColak <feride_colak577@hotmail.com>
2022-08-26 16:20:02 +0300

    UpdateProductCommandResponse sınıfı eklendi.

---
+++ CqrsRepositoryMediatR/Cqrs/Commands/Responses/UpdateProductCommandResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CqrsRepositoryMediatR.Cqrs.Commands.Responses
+{
+    public class UpdateProductCommandResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+    }
+}
