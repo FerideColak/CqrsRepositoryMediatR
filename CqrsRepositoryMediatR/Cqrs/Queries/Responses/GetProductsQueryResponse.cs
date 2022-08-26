d69366eeb73c718db81d75bcf0e66e0ae47874e5
FerideColak <feride_colak577@hotmail.com>
2022-08-26 16:22:27 +0300

    QueryResponse sınıfları oluşturuldu.

---
+++ CqrsRepositoryMediatR/Cqrs/Queries/Responses/GetProductsQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CqrsRepositoryMediatR.Cqrs.Queries.Responses
+{
+    public class GetProductsQueryResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+    }
+}
