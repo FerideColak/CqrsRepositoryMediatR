fbc069c3ab6db02f91816b9346b3a9c094919676
FerideColak <feride_colak577@hotmail.com>
2022-08-26 14:38:20 +0300

    Product Entity eklendi.

---
+++ CqrsRepositoryMediatR/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace CqrsRepositoryMediatR.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+    }
+}
